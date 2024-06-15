3fdaf292c5574c92d330f32dc7d876e00932816a
Sose <@sose>
2024-06-15 14:11:54 +0200

    hardcode width option

---
+++ src/lib/components/core/Preview.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let svgContent = "";
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<div class="svgWrapper">
+  {@html svgContent}
+</div>
+
+<style>
+  .svgWrapper {
+    border: 1px solid crimson;
+  }
+</style>
