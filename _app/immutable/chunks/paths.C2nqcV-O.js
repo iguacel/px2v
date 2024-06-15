bec3265414d34f72e02f3e45e2f58dbb31686f58
Sose <@sose>
2024-06-15 10:08:04 +0200

    Updates

---
+++ _app/immutable/chunks/paths.C2nqcV-O.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1f5haky)==null?void 0:s.base)??"/px2v";var a;const t=((a=globalThis.__sveltekit_1f5haky)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.Bm4SiClm.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bop7UovU.js";export{t as start};
